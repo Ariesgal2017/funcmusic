7510183961b8c7f96ddcb2651444d3484128f745
Jennifer Schneider <ariesgal2917@gmail.com>
2020-12-15 03:59:47 -0800

    finished function music

---
+++ function_music.py
@@ -0,0 +1,24 @@
+"Genre", "Artist" and "Year"
+
+def Genre():
+    type_of_music = "Rap"
+    print(type_of_music)
+Genre()
+
+
+def Artist():
+    rapper_name = "Eminem/Marshall Mathers"
+    print(rapper_name)
+Artist()
+
+
+def Year():
+    year_released = "2001 - current"
+    print(year_released)
+Year()
+
+
+def Best():
+    best_ever = True
+    print(best_ever)
+Best()
